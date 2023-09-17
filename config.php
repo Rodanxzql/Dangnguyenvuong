a64f4d3db1c89b2546c51bec98d0b72d86a9541a
Rodanxzql <92496910+Rodanxzql@users.noreply.github.com>
2023-09-18 01:41:09 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define("HOST_DB", 'dbgenshin.cmywhljzxxnf.ap-southeast-1.rds.amazonaws.com');
+define("USER_DB", 'admin');
+define("PASS_DB", '01682998982');
+define("DB", 'book');
+
+define ('ROOT', dirname(__FILE__));
+define('BASE_URL', 'http://localhost:8080/mvc3/');
+define('UPLOAD_IMG', ROOT .'/images/book');
+
